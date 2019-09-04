fa6b5bc5aa9e4e8653f4a649cb5472d7edf54dcf
Kevin Lane <mudcitizen@gmail.com>
2019-09-03 21:00:02 -0400

    Generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,24 @@ import { AppComponent } from './app.component';
 import { FinderComponent } from './components/finder/finder.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
-import {StaticDataProvider} from "./data/providers/static.provider"
-import { Injection_Token_DataProvider} from "./common/constants"
+import { StaticDataProvider } from "./data/providers/static.provider"
+import { Injection_Token_DataProvider } from "./common/constants"
 import { DataService } from './data/service/data.service';
+import { GuestsComponent } from './components/guests/guests.component';
+import { GuestComponent } from './components/guest/guest.component';
+import { ReservationComponent } from './components/reservation/reservation.component';
+import { ReservationsComponent } from './components/reservations/reservations.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FinderComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    GuestsComponent,
+    GuestComponent,
+    ReservationComponent,
+    ReservationsComponent
   ],
   imports: [
     BrowserModule,
