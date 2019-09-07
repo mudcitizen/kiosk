2739a552ef668975bd69eb9a0e95eb2711f63e87
Kevin Lane <mudcitizen@gmail.com>
2019-09-06 20:30:31 -0400

    Changes to get a successful test under Karma - TestBed.configureTestingModule() stuff

--- src/app/components/finder/finder.component.spec.ts
+++ src/app/components/finder/finder.component.spec.ts
@@ -1,16 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
 import { FinderComponent } from './finder.component';
+import { StaticDataProvider } from 'src/app/data/providers/static.provider';
+import { DataService } from 'src/app/data/service/data.service';
+import { Injection_Token_DataProvider } from 'src/app/common/constants';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { AppModule } from 'src/app/app.module';
+import { APP_BASE_HREF } from '@angular/common';
 
 describe('FinderComponent', () => {
   let component: FinderComponent;
   let fixture: ComponentFixture<FinderComponent>;
 
   beforeEach(async(() => {
+    
     TestBed.configureTestingModule({
-      declarations: [ FinderComponent ]
+
+      imports: [FormsModule,
+        AppModule,
+        AppRoutingModule],
+
+      providers: [
+        { provide: Injection_Token_DataProvider, useValue: new StaticDataProvider() },
+        { provide: DataService, useClass: DataService },
+        { provide: APP_BASE_HREF, useValue: '/' }
+      ]
+
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
