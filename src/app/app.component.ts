eeef308375792bec86c072f1cdfd1d65a77e6f9f
Kevin Lane <mudcitizen@gmail.com>
2019-09-10 20:59:48 -0400

    Get automapper installed / configured and working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GuestData} from "./data/models/Guest.data";
+import { Guest} from "./data/models/Guest.model";
+import 'automapper-ts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+//automapper.createMap(GuestData,Guest);
+export class AppComponent implements OnInit {
   title = 'Kiosk';
+  ngOnInit() {
+    automapper.createMap(GuestData,Guest);
+  }
 }
